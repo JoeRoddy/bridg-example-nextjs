671352dc9cb305e5040bac93a753893918f9385a
Joe Roddy <mrjoeroddy@gmail.com>
2023-04-15 15:30:33 -0500

    useAsync hook for simplicity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,20 @@
-import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { useAsync } from '@/hooks/useAsync';
 import db from 'bridg/app/client/db';
 import { DbRules } from 'bridg/app/server/request-handler';
+import { NextPage } from 'next';
 
 const BridgExample: NextPage = ({}) => {
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      // Query your DB from the frontend 😎
-      const data = await db.user.findMany({
-        // filter your results
-        where: {
-          email: { contains: '@prisma.io' },
-        },
-        // include related data
-        include: { blogs: true },
-      });
-
-      setData(data);
-    })();
-  }, []);
+  const data = useAsync(() =>
+    // Query your DB from the frontend 😎
+    db.user.findMany({
+      // filter your results
+      where: {
+        email: { contains: '@prisma.io' },
+      },
+      // include related data
+      include: { blogs: true },
+    }),
+  );
 
   return data === undefined ? <GetStarted /> : <pre>{JSON.stringify(data, null, 1)}</pre>;
 };
